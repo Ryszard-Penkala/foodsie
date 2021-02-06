b63b4f8d54155aae96d25504c4c6898bae33724d
Ryszard Penkała <ryszard.penkala@gmail.com>
2021-02-06 16:07:23 +0100

    card animation added

---
+++ .history/src/components/BottomContentWrapper/BottomContentWrapper_20210206155154.js
@@ -0,0 +1,18 @@
+import Card from '../Card/Card';
+import styles from './BottomContentWrapper.module.scss';
+
+
+const BottomContentWrapper = () => {
+
+    return (
+        <>
+            <div className = { [styles.bottomWrapper, styles.bounce] } >
+                <Card cardTitle = 'foodRecipes' />
+                <Card cardTitle = 'workouts' />
+                <Card cardTitle = 'library' />
+            </div>
+        </>
+    );
+}
+
+export default BottomContentWrapper;
